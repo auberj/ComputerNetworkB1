09ac68146724df9cd1cea4faf599fe11d0118430
auberj <nathan@Nathans-MacBook-Pro.local>
2015-11-06 13:14:32 +0000

    updates

--- network_layer/physical_dummy/physical.cpp
+++ network_layer/physical_dummy/physical.cpp
@@ -6,7 +6,22 @@ int SendPacket(char dest, char* packet){
 	return 0;
 }
 
-int RecievePacket(char source, char* packet){
+int RecievePacket(char* packet){
+	char dummy_packet[128];
 
+	dummy_packet[0] = 0xFF;
+	dummy_packet[1] = 0xFF;
+	//set SRC address
+	dummy_packet[2] = 0x02;
+	//set DEST address (doesn't matter for HELLO)
+	dummy_packet[3] = 0x01;
+	//set length
+	dummy_packet[4] = MaxSegmentLength;
+	//set segment to be empty
+	for(int i=5;i<125;i++){
+		dummy_packet[i] = 'i';
+	}
+	dummy_packet[126] = 0x00;
+	dummy_packet[127] = 0x00;
 	return 0;
 }

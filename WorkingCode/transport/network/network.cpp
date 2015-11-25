fd82de1a57ad249dd99afb44d82219b14cc91273
Dominic Maskell <dommaskell@hotmail.co.uk>
2015-11-25 22:13:43 +0000

    Backed up transport as it's good enough to be marked now...

---
+++ WorkingCode/transport/network/network.cpp
@@ -0,0 +1,16 @@
+#include <string.h>
+
+int SendSegment(char dest, char* ssegment)
+{
+	// display_string("Sending segment to ");
+	// display_char(dest);
+	// display_string(" with message: ");
+	// display_nstring(ssegment, 0, 120);
+	// display_char('\n');
+	return 0;
+}
+
+// int RecieveSegment(char source, char* rsegment)
+// {
+
+// }

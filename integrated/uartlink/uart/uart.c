a23698206e0e7e99e5636d7be144014c42501791
Dominic Maskell <dommaskell@hotmail.co.uk>
2015-12-07 16:17:23 +0000

    using uart instead

---
+++ integrated/uartlink/uart/uart.c
@@ -0,0 +1,75 @@
+//Author: dm6g13
+
+#include "uart.h"
+#include <stdio.h>
+
+void init_uart0(void) //Function copied from Steve Gunn's library
+{
+	/* Configure 9600 baud, 8-bit, no parity and one stop bit */
+	const int baud_rate = 9600;
+	UBRR0H = (F_CPU/(baud_rate*16L)-1) >> 8;
+	UBRR0L = (F_CPU/(baud_rate*16L)-1);
+	UCSR0B = _BV(RXEN0) | _BV(TXEN0);
+	UCSR0C = _BV(UCSZ00) | _BV(UCSZ01);
+}
+
+char get_char(void) //Function copied from Steve Gunn's library
+{
+	while(!(UCSR0A & _BV(RXC0)));
+	return UDR0;
+}
+
+void put_char(char ch) //Function copied from Steve Gunn's library
+{
+	while (!(UCSR0A & _BV(UDRE0)));
+	UDR0 = ch;
+}
+
+void put_string(char *str) //Function copied from Steve Gunn's library
+{
+	int i;
+	for(i=0; str[i]; i++) put_char(str[i]);
+}
+
+void put_nstring(char *str, uint8_t start, uint8_t n) //Displays n characters in the string from start point called 'start'
+{
+	uint8_t i;
+	for(i=start; i<(start+n); i++) 
+		put_char(str[i]);
+}
+
+void put_number(uint16_t number) //Displays a number using sprintf. Can display between 0 and 99999999
+{
+	char numberstring[8];
+  	snprintf(numberstring, 8, "%d", number); //Convert to string so we can display
+  	put_string(numberstring);
+}
+
+void put_hex(unsigned long hex, uint8_t bytes) //Displys 1 to 4 bytes in hex (if you want up to 8, change 'long'to 'long long')
+{
+	char hexchars[bytes*2];
+	for (int i = 0; i < bytes*2; i++)
+	{
+		uint8_t temp = 0xF & (hex >> (i*4));
+		if (temp >= 10) //A to F
+			hexchars[bytes*2-1-i] = temp + 87;
+		else
+			hexchars[bytes*2-1-i] = temp + 48;
+	}
+	put_string("0x");
+	put_nstring(hexchars, 0, bytes*2);
+}
+
+void put_binary(uint8_t byte) //Displys a byte in binary
+{
+	char binchars[8];
+	for (int i = 7; i >= 0; i--)
+	{
+		if (byte & (1 << i))
+			binchars[7-i]='1';
+		else
+			binchars[7-i]='0'; 
+	}
+    put_string("0b");
+    put_nstring(binchars, 0, 8);
+}
---
+++ integrated/uartlink/uartlink.cpp
@@ -0,0 +1,11 @@
+#include "i2c/i2c.c"
+
+int SendPacket(char dest, char* spacket)
+{
+	init_i2c_master();
+}
+
+int RecievePacket(char* rpacket)
+{
+
+}
